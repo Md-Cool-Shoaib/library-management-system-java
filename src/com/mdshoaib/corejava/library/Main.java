82f387c893d992811dde6fe443b0b1bcfc4c4704
md-shoaib-424 <shoaibmohmmad560@gmail.com>
2026-03-01 10:19:20 +0530

    Add Main class to demonstrate Library Management System workflow

---
+++ src/com/mdshoaib/corejava/library/Main.java
@@ -0,0 +1,36 @@
+package com.mdshoaib.corejava.library;
+
+public class Main {
+    public static void main(String[] args) {
+        //Created a library with space exactly 3.
+        Library myLibrary = new Library(3);
+        //Created some book object.
+        Book book1 = new Book("Introduction to Algorithms", "Thomas H. Cormen",121);
+        Book book2 = new Book("Clean Code", "Robert C. Martin",122);
+        Book book3 = new Book("Database System Concepts", "Abraham Silberman",123);
+        Book book4 = new Book("Design Patterns", "Erich Gamma",124);
+        //Add book to the library.
+        try{
+            System.out.println("Adding books...");
+            myLibrary.addBook(book1);
+            myLibrary.addBook(book2);
+            myLibrary.addBook(book3);
+            myLibrary.addBook(book4);
+        }catch (IllegalAccessError e){
+            System.out.println("Caught a state error: "+ e.getMessage());
+        }catch (NullPointerException e){
+            System.out.println("Caught a null error: "+ e.getMessage());
+        }catch (Exception e){
+            System.out.println("An error occurred: "+ e.getMessage());
+        }finally {
+
+            System.out.println("\n---- Current Library Catalog ----");
+            myLibrary.showAllBooks();
+        }
+
+        System.out.println(myLibrary.searchByTitle("Database System Concepts"));
+        myLibrary.removeBook("Database System Concepts");
+        System.out.println(myLibrary.bookCount);
+
+    }
+}
